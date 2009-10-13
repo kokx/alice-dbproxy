88a027f90e8de0fb521ce45f074cc8cd8d490fd2
Pieter Kokx <pieter@kokx.nl>
2009-10-13 10:47:22 +0200

    Added GPL License.

--- config.php
+++ config.php
@@ -1,4 +1,32 @@
 <?php
+/**
+ * Alice DBProxy
+ * 
+ * LICENSE
+ * 
+ * Alice DBProxy - Database proxy for Alice
+ * Copyright (C) 2009  Pieter Kokx <pieter@kokx.nl>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * 
+ * @author   Pieter Kokx <pieter@kokx.nl>
+ * @category Alice
+ * @license  http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt GPL version 2
+ * @package  Alice_DBProxy
+ */
+
 return array(
     'db' => array(
         'dsn'      => 'mysql:hostname=localhost;dbname=alice',
